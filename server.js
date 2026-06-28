492d9d5bf903b3ca26819b6ecf82e0ef86fc9382
nyashstudios-sudo <nyashstudios@gmail.com>
2026-06-28 17:49:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+const express = require('express');
+const path = require('path');
 const jwt = require('jsonwebtoken');
 
 const app = express();
